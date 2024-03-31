e1a4caee5e93f567d9cdd72162a5f53da68cb3c3
RUKHSAR KHURSHEED <116160178+rukhsar118@users.noreply.github.com>
2024-03-31 14:47:53 +0000

    Data Types

--- 01-basics/dataTypes.js
+++ 01-basics/dataTypes.js
@@ -5,3 +5,42 @@
 
 
 console.log(3+3)   // code readablity should be high
+
+// Primitive data types
+
+let age = 19
+//number => 2 to power 53
+console.log(typeof (age))   //number
+
+//bigint for big number 
+
+//string = ""   
+ let name = "Luckey"
+ console.log(typeof (name))  // string
+
+
+// boolean => true/false  
+ let isLoggedIn = true
+ console.log(typeof (isLoggedIn))  // boolean
+
+
+
+//null => standalone value  
+ let state = null // its not undefined but has a value which is null/ empty
+ console.log(typeof (state))  //object
+
+
+// undefined => when  you have declared a variable but you have not defined the value
+ let car;
+ console.log(typeof (car))  // undefined
+
+ 
+
+ //symbol => unique / will study further in react
+
+
+
+
+
+
+ 
